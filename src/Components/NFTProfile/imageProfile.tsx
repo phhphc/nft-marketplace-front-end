d6926942b378662975e1096c58ab684ab061277a
GrexThinh <vanthinhnq@gmail.com>
2023-01-17 22:09:49 +0700

    done hard code

---
+++ src/Components/NFTProfile/imageProfile.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from "react";
+
+const ImageProfile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleShowDialog = () => {
+    setIsOpen(!isOpen);
+  };
+
+  // const wrapperRef = useRef<any>(null);
+  // useEffect(() => {
+  //   document.addEventListener("click", handleClickOutside, false);
+  //   return () => {
+  //     document.removeEventListener("click", handleClickOutside, false);
+  //   };
+  // }, []);
+
+  // const handleClickOutside = (event: any) => {
+  //   if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+  //     setIsOpen(false);
+  //   }
+  // };
+  return (
+    <div id="nft-profile">
+      <img
+        src="https://i.seadn.io/gae/mmyoMHBLnHMfHyb3r2T1050yScZqfx2G48kXmP6WruMNpJNnVYsD79tpczbCrAilRLCAkUh3qRTRdpHYx5z9QPnLG3tdXoTU_Hc9?auto=format&w=1920"
+        alt=""
+        className="cover-image"
+      ></img>
+      <div className="avt-frame">
+        <img
+          onClick={handleShowDialog}
+          src="https://i.seadn.io/gae/FeqZQ2fAjaNqCrxpz2x9ZlNR9PXAP6Ok-lwnaX6ti-BomzyEKmnslCZorU1-aKWM_zq6Mop7RJHT_YqVEKBXh9PIOEciNlxnQYjl?auto=format&w=256"
+          alt=""
+          className="shadow-2xl avt-image"
+        ></img>
+      </div>
+      {/* Avatar NFT Modal */}
+      <div className={isOpen ? "modal" : ""}>
+        <span
+          onClick={handleShowDialog}
+          className={isOpen ? "close" : "hidden"}
+        >
+          &times;
+        </span>
+        <img
+          className="modal-content"
+          src={
+            isOpen
+              ? "https://i.seadn.io/gae/FeqZQ2fAjaNqCrxpz2x9ZlNR9PXAP6Ok-lwnaX6ti-BomzyEKmnslCZorU1-aKWM_zq6Mop7RJHT_YqVEKBXh9PIOEciNlxnQYjl?auto=format&w=256"
+              : ""
+          }
+          // ref={wrapperRef}
+        />
+      </div>
+    </div>
+  );
+};
+export default ImageProfile;
--- src/Containers/NFTCollectionContainer/NFTCollectionContainer.tsx
+++ src/Containers/NFTCollectionContainer/NFTCollectionContainer.tsx
@@ -1,8 +1,12 @@
 import NFTCollectionList from "@Components/NFTCollectionList/NFTCollectionList";
+import ImageProfile from "@Components/NFTProfile/imageProfile";
+import NFTInfor from "@Components/NFTProfile/NFTInfor";
 
 const NFTCollectionContainer = () => {
   return (
     <>
+      <ImageProfile></ImageProfile>
+      <NFTInfor></NFTInfor>
       <NFTCollectionList />
     </>
   );
