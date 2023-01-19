051070c5e0dec91d5a509c9bf0fcf619a783791d
hhddang <dangtramthien@gmail.com>
2023-01-19 09:27:39 +0700

    NFT Detail UI

---
+++ src/Components/NFTDetail/mockData.ts
@@ -0,0 +1,22 @@
+import { INFTDetail } from "./NFTDetail";
+
+export const nftDetail: INFTDetail = {
+    imageSrc:
+        "https://i.seadn.io/gae/rxhWr6EWSG-izM8ip8CRA6rEeuQOa9iD5j7ZFQtAYEOGEyaGx0nAyFDMhUf6GZiqT8Wqvll9tW3gOKK0-ALoAXR47PwRDegUTnRr?auto=format&w=512",
+    author: "TRISTAN EATON",
+    name: "GEMMA",
+    id: "#2488",
+    owner: "DC4B58",
+    view: 278,
+    favorite: 11,
+    category: "PFPs",
+    sale: {
+        end: "January 20, 2023 at 8:50 AM GMT+7",
+        day: "02",
+        hour: "09",
+        minute: "17",
+        second: "02",
+    },
+    priceEth: 0.2,
+    priceDol: 316.92,
+};
