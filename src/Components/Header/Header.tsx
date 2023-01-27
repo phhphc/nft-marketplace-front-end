7e157f7df7178bce0217b36495a37189c81d8e07
hhddang <dangtramthien@gmail.com>
2023-01-27 20:29:25 +0700

    Done

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,5 @@
 const Header = () => {
-  return (
-    <div className="fixed top-0 right-0 left-0 bg-blue-600 h-24 z-10"></div>
-  );
+    return <div className="fixed top-0 right-0 left-0 bg-blue-600 h-24 z-10">abc</div>;
 };
 
 export default Header;
