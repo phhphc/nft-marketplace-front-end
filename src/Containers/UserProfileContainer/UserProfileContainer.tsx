d55a8a9e91623f89e7908995cc79c67e69c042b4
hhddang <dangtramthien@gmail.com>
2023-01-27 20:36:45 +0700

    Done

--- src/Containers/UserProfileContainer/UserProfileContainer.tsx
+++ src/Containers/UserProfileContainer/UserProfileContainer.tsx
@@ -1,16 +1,16 @@
-// import NFTCollectionList from "@Components/NFTCollectionList/NFTCollectionList";
+import NFTUserProfileTabs from "@Components/NFTUserProfileTabs/NFTUserProfileTabs";
 
 import ImageUserProfile from "@Components/UserProfile/ImageUserProfile";
 import UserInfor from "@Components/UserProfile/UserInfor";
 
 const UserProfileContainer = () => {
-  return (
-    <>
-      <ImageUserProfile></ImageUserProfile>
-      <UserInfor></UserInfor>
-      {/* <NFTCollectionList /> */}
-    </>
-  );
+    return (
+        <>
+            <ImageUserProfile></ImageUserProfile>
+            <UserInfor></UserInfor>
+            <NFTUserProfileTabs />
+        </>
+    );
 };
 
 export default UserProfileContainer;
