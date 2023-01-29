7a71e6f316a0d2990527e59d0ec6dc3575fd818a
HuynhNgocBao <baohuynhxayda@gmail.com>
2023-01-29 21:55:59 +0700

    Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,5 +1,30 @@
+import logo from "@Assets/logo.png";
+import Image from "next/image";
+
 const Footer = () => {
-  return <div className="w-full bg-blue-600 h-24"></div>;
+  return (
+    <div className="w-full grid grid-cols-2 border-t border-purple p-16 items-center bg-gray-100">
+      <div className="flex flex-col">
+        <Image src={logo} alt="logo" className="w-36 h-16 self-center mb-4" />
+        <div className="text-justify">
+          The world's first and largest digital marketplace for crypto
+          collectibles and non-fungible tokens (NFTs). Buy, sell and discover
+          exclusive digital items.
+        </div>
+      </div>
+      <div className="flex justify-center self-center">
+        <div className="mr-16">
+          <div className="font-bold">Marketplace</div>
+          <div className="pt-4">All NFTs</div>
+        </div>
+        <div className="mr-16">
+          <div className="font-bold">My Account</div>
+          <div className="pt-4">Profile</div>
+          <div className="pt-4">My Collections</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,45 +1,51 @@
 import Image from "next/image";
-import logo from "@Components/Header/logo.png";
-import logo2 from "@Components/Header/logo2.png";
+import logo from "@Assets/logo.png";
+import logo2 from "@Assets/logo2.png";
 import { InputText } from "primereact/inputtext";
 import { Tooltip } from "primereact/tooltip";
 import Link from "next/link";
 
 const Header = () => {
-    return (
-        <div id="header" className="fixed top-0 right-0 left-0 h-24 z-10">
-            <div className="flex h-full w-full bg-black shadow items-center justify-between px-10">
-                <Link href="/">
-                    <Image id="logo" className="h-16 w-36" src={logo2} alt="logo" />
-                </Link>
-                <span id="search-bar" className="p-input-icon-left">
-                    <i className="pi pi-search" />
-                    <InputText placeholder="Search" className="w-96 h-10" />
-                </span>
-                <div id="navbar" className="w-56 flex items-center justify-around">
-                    <Link href="/user-profile">
-                        <Tooltip target="#avatar" position="bottom">
-                            Profile
-                        </Tooltip>
-                        <img
-                            id="avatar"
-                            src="https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/278374293_156929620076225_8622926127986152733_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=za5rCUtfJx4AX8otllQ&_nc_ht=scontent.fsgn1-1.fna&oh=00_AfDhdFjbMAG-3FTKtOlGlqSH1C4lJENi09ZU-3RoFKe9MA&oe=63D8AB0A"
-                            alt="avatar"
-                            className="h-10 w-10 border-2 border-yellow rounded-full"
-                        ></img>
-                    </Link>
-                    <Tooltip target="#wallet" position="bottom">
-                        Wallet
-                    </Tooltip>
-                    <i id="wallet" className="pi pi-wallet text-white text-3xl cursor-pointer"></i>
-                    <Tooltip target="#cart" position="bottom">
-                        Your cart
-                    </Tooltip>
-                    <i id="cart" className="pi pi-cart-plus text-white text-3xl cursor-pointer"></i>
-                </div>
-            </div>
+  return (
+    <div id="header" className="fixed top-0 right-0 left-0 h-24 z-10">
+      <div className="flex h-full w-full bg-black shadow items-center justify-between px-10">
+        <Link href="/">
+          <Image id="logo" className="h-16 w-36" src={logo2} alt="logo" />
+        </Link>
+        <span id="search-bar" className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText placeholder="Search" className="w-96 h-10" />
+        </span>
+        <div id="navbar" className="w-56 flex items-center justify-around">
+          <Link href="/user-profile">
+            <Tooltip target="#avatar" position="bottom">
+              Profile
+            </Tooltip>
+            <img
+              id="avatar"
+              src="https://scontent.fsgn1-1.fna.fbcdn.net/v/t39.30808-6/278374293_156929620076225_8622926127986152733_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=za5rCUtfJx4AX8otllQ&_nc_ht=scontent.fsgn1-1.fna&oh=00_AfDhdFjbMAG-3FTKtOlGlqSH1C4lJENi09ZU-3RoFKe9MA&oe=63D8AB0A"
+              alt="avatar"
+              className="h-10 w-10 border-2 border-yellow rounded-full"
+            ></img>
+          </Link>
+          <Tooltip target="#wallet" position="bottom">
+            Wallet
+          </Tooltip>
+          <i
+            id="wallet"
+            className="pi pi-wallet text-white text-3xl cursor-pointer"
+          ></i>
+          <Tooltip target="#cart" position="bottom">
+            Your cart
+          </Tooltip>
+          <i
+            id="cart"
+            className="pi pi-cart-plus text-white text-3xl cursor-pointer"
+          ></i>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
