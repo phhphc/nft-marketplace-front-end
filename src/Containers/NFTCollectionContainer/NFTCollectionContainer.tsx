cae8f0132ec64af659f35699434b226088419a0b
HuynhNgocBao <baohuynhxayda@gmail.com>
2023-02-11 11:02:38 +0700

    Render metadata NFTInfor and ImageProfile

--- src/Containers/NFTCollectionContainer/NFTCollectionContainer.tsx
+++ src/Containers/NFTCollectionContainer/NFTCollectionContainer.tsx
@@ -3,10 +3,10 @@ import ImageProfile from "@Components/NFTProfile/ImageProfile";
 import NFTInfor from "@Components/NFTProfile/NFTInfor";
 import { NFT_COLLECTION_MODE } from "@Constants/index";
 import { getNFTCollectionListService } from "@Services/ApiService";
-import { useState, useEffect, useContext, useRef} from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { INFTCollectionItem } from "@Interfaces/index";
 import { AppContext } from "@Store/index";
-import { Toast } from "primereact/toast"
+import { Toast } from "primereact/toast";
 
 const NFTCollectionContainer = () => {
   const [nftCollectionList, setNftCollectionList] = useState<
@@ -14,19 +14,23 @@ const NFTCollectionContainer = () => {
   >([]);
   const [countFetchNftCollectionList, setCountFetchNftCollectionList] =
     useState<number>(0);
-  
-  const toast = useRef(null);
+
+  const toast = useRef<any>(null);
 
   const web3Context = useContext(AppContext);
-  
+
   useEffect(() => {
     const fetchData = async () => {
       getNFTCollectionListService().then((data) => {
-        if(data){
+        if (data) {
           setNftCollectionList(data.nfts);
-        }
-        else{
-          toast.current.show({severity:'error', summary: 'Error', detail:'Fail to load collections', life: 3000});
+        } else {
+          toast.current.show({
+            severity: "error",
+            summary: "Error",
+            detail: "Fail to load collections",
+            life: 3000,
+          });
         }
       });
     };
@@ -34,25 +38,21 @@ const NFTCollectionContainer = () => {
     fetchData();
   }, [countFetchNftCollectionList]);
   return (
-    
     <>
-      {web3Context.state.web3.provider ?
-      (<>
-        <Toast ref={toast} position="bottom-right" />
-        <ImageProfile></ImageProfile>
-        <NFTInfor></NFTInfor>
-        <NFTCollectionList
-          nftCollectionList={nftCollectionList}
-          mode={NFT_COLLECTION_MODE.CAN_BUY}
-          setCountFetchNftCollectionList={setCountFetchNftCollectionList}
-        />
-        </>
-      ):(
+      {web3Context.state.web3.provider ? (
         <>
-        Please login
+          <Toast ref={toast} position="bottom-right" />
+          <ImageProfile nftCollectionList={nftCollectionList}></ImageProfile>
+          <NFTInfor nftCollectionList={nftCollectionList}></NFTInfor>
+          <NFTCollectionList
+            nftCollectionList={nftCollectionList}
+            mode={NFT_COLLECTION_MODE.CAN_BUY}
+            setCountFetchNftCollectionList={setCountFetchNftCollectionList}
+          />
         </>
-      )
-      }
+      ) : (
+        <>Please login</>
+      )}
     </>
   );
 };
