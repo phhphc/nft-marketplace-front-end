95838ad7085d210b84b64f6aa05b8dd27d3a9e99
HuynhNgocBao <baohuynhxayda@gmail.com>
2023-04-02 12:50:16 +0700

    fix bug not rendering nft detail

--- src/Hooks/useNFTCollectionList.ts
+++ src/Hooks/useNFTCollectionList.ts
@@ -1,7 +1,5 @@
 import { getNFTCollectionListInfoService } from "@Services/ApiService";
 import { useQuery } from "react-query";
-import { NOT_ON_SALE } from "@Constants/index";
-import { INFTCollectionItem } from "@Interfaces/index";
 
 const useNFTCollectionList = () => {
   const result = useQuery({
@@ -10,22 +8,7 @@ const useNFTCollectionList = () => {
     staleTime: Infinity,
     retry: true,
   });
-  const nftCollectionList =
-    result.data?.reduce((acc: any, cur: INFTCollectionItem) => {
-      const indexOfExistOrderHash = acc.indexOf(
-        (item: INFTCollectionItem) =>
-          item.listings?.[0].order_hash === cur.listings?.[0].order_hash
-      );
-
-      if (indexOfExistOrderHash === -1) {
-        acc.push([cur]);
-        return acc;
-      } else {
-        acc[indexOfExistOrderHash].push(cur);
-        return acc;
-      }
-    }, []) || [];
-  console.log(nftCollectionList);
+  const nftCollectionList = result.data || [];
   return { ...result, nftCollectionList };
 };
 
