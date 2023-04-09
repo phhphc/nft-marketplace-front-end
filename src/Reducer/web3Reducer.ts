185e10e36c4bad4b72012be9a606bff219407500
HuynhNgocBao <baohuynhxayda@gmail.com>
2023-04-09 18:20:25 +0700

    fix table

--- src/Reducer/web3Reducer.ts
+++ src/Reducer/web3Reducer.ts
@@ -7,6 +7,32 @@ const web3Reducer = (state: IWeb3, action: IWeb3Action) => {
         ...state,
         ...action.payload,
       };
+      break;
+    case WEB3_ACTION_TYPES.ADD_BUNDLE:
+      const newAddBundle = [
+        ...state.listItemsSellBundle,
+        action.payload,
+      ].slice();
+      return {
+        ...state,
+        listItemsSellBundle: newAddBundle,
+      };
+      break;
+    case WEB3_ACTION_TYPES.REMOVE_BUNDLE:
+      const newRemoveBundle = state.listItemsSellBundle
+        .filter((item) => item.identifier !== action.payload)
+        .slice();
+      return {
+        ...state,
+        listItemsSellBundle: newRemoveBundle,
+      };
+      break;
+    case WEB3_ACTION_TYPES.SET_BUNDLE:
+      return {
+        ...state,
+        listItemsSellBundle: action.payload,
+      };
+      break;
     default:
       return state;
   }
