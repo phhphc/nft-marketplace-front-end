19f0978d2aa130933fdbaa0386d6f4747a2e52db
HuynhNgocBao <bhuynh@swellenergy.com>
2023-05-17 14:43:34 +0700

    fix taking min price

--- src/Hooks/useNFTCollectionList.ts
+++ src/Hooks/useNFTCollectionList.ts
@@ -1,5 +1,6 @@
-import { INFTCollectionItem } from "@Interfaces/index";
+import { IListing, INFTCollectionItem } from "@Interfaces/index";
 import { getNFTCollectionListService } from "@Services/ApiService";
+import { cloneDeep } from "lodash";
 import { useQuery } from "react-query";
 
 interface IUseNFTCollectionListProps {
@@ -20,27 +21,45 @@ const useNFTCollectionList = ({
   const result = useQuery({
     queryKey: `nftCollectionList-${token}-${owner}-${isHidden}-${provider}-${myWallet}`,
     queryFn: () =>
-      getNFTCollectionListService({ token, owner, isHidden }, provider, myWallet),
+      getNFTCollectionListService(
+        { token, owner, isHidden },
+        provider,
+        myWallet
+      ),
     staleTime: Infinity,
     retry: true,
   });
 
   const nftCollectionList: INFTCollectionItem[][] =
-    result.data?.reduce((acc: any, cur: INFTCollectionItem) => {
-      const indexOfExistOrderHash = acc.findIndex(
-        (item: INFTCollectionItem[]) =>
-          item[0].listings?.[0]?.order_hash &&
-          item[0].listings?.[0]?.order_hash === cur.listings?.[0]?.order_hash
-      );
+    result.data
+      ?.map((item: INFTCollectionItem) => {
+        const newListings = cloneDeep(item.listings);
+        return cloneDeep({
+          ...item,
+          listings: newListings.sort(
+            (listingA: IListing, listingB: IListing) => {
+              return (
+                Number(listingA.start_price) - Number(listingB.start_price)
+              );
+            }
+          ),
+        });
+      })
+      .reduce((acc: any, cur: INFTCollectionItem) => {
+        const indexOfExistOrderHash = acc.findIndex(
+          (item: INFTCollectionItem[]) =>
+            item[0].listings?.[0]?.order_hash &&
+            item[0].listings?.[0]?.order_hash === cur.listings?.[0]?.order_hash
+        );
 
-      if (indexOfExistOrderHash === -1) {
-        acc.push([cur]);
-        return acc;
-      } else {
-        acc[indexOfExistOrderHash].push(cur);
-        return acc;
-      }
-    }, []) || [];
+        if (indexOfExistOrderHash === -1) {
+          acc.push([cur]);
+          return acc;
+        } else {
+          acc[indexOfExistOrderHash].push(cur);
+          return acc;
+        }
+      }, []) || [];
 
   return { ...result, nftCollectionList };
 };
