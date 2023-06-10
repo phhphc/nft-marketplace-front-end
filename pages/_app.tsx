6326524114b684f60da3c89c1af1e1b4fe4d95cb
HuynhNgocBao <baohuynhxayda@gmail.com>
2023-06-10 09:46:46 +0700

    init admin and mod page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import MainLayout from "@Layouts/MainLayout/MainLayout";
 import "@Styles/index.scss";
 import { AppProvider } from "@Store/index";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 
 declare global {
   interface Window {
@@ -10,15 +12,22 @@ declare global {
   }
 }
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout =
+    Component.getLayout || ((page) => <MainLayout>{page}</MainLayout>);
+
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
-      <AppProvider>
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </AppProvider>
+      <AppProvider>{getLayout(<Component {...pageProps} />)}</AppProvider>
     </QueryClientProvider>
   );
 }
