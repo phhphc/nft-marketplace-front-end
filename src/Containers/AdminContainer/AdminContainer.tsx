8fae1683ada1ad65e10d87baf9d7a485ff7e6f74
GrexThinh <vanthinhnq@gmail.com>
2023-06-18 21:42:32 +0700

    setting admin

--- src/Containers/AdminContainer/AdminContainer.tsx
+++ src/Containers/AdminContainer/AdminContainer.tsx
@@ -1,5 +1,6 @@
 import AdminManagement from "@Components/AdminManagement/AdminManagement";
 import useAllUsers from "@Hooks/useAllUsers";
+import useGetMkpInfo from "@Hooks/useGetMkpInfo";
 import { AppContext } from "@Store/index";
 import { useContext } from "react";
 
@@ -8,11 +9,16 @@ const AdminContainer = () => {
   const { users, refetch: usersRefetch } = useAllUsers(
     web3Context.state.web3.chainId
   );
+  const { mkpInfo, refetch: mkpInfoRefetch } = useGetMkpInfo(
+    web3Context.state.web3.chainId
+  );
   return (
     <>
       <AdminManagement
         users={users}
         usersRefetch={usersRefetch}
+        mkpInfo={mkpInfo}
+        mkpInfoRefetch={mkpInfoRefetch}
       ></AdminManagement>
     </>
   );
